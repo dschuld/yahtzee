2431c2eb485ff4ae0b1504940518600d99e630d7
David <davidschuld@gmail.com>
2018-01-27 14:05:01 +0200

    Extracted CSS and JS files

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,4 +1,29 @@
-setTimeout(function(){
-   $("#passiveOnes").html("11");
-   <!--createTable();-->
-}, 50000);
+startUpdate = function() {
+
+  var updateFields = $(".update");
+  var playerName = getPassivePlayerName();
+    var update = function(){
+
+
+        updateFields.each(function(element) {
+            var field = this;
+            var path = "value/" + playerName + "/" + this.id;
+            $.get(path, function( data ) {
+                field.innerHTML = data;
+             });
+
+        });
+
+         $.get("roll",  function( data ) {
+            $("#dice1")[0].innerHTML = data.dice1;
+            $("#dice2")[0].innerHTML = data.dice2;
+            $("#dice3")[0].innerHTML = data.dice3;
+            $("#dice4")[0].innerHTML = data.dice4;
+            $("#dice5")[0].innerHTML = data.dice5;
+            $("#remainingRolls")[0].innerHTML = getRemainingRollsText() +': ' +data.remainingRolls;
+         });
+
+    }
+update();
+setInterval(update, 3000);
+}
